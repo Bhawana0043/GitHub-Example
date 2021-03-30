18e9ac4237a2eab8cb92c85b892bf9c73b98c5fe
Bhawana Gautam <bhawanag0255@gmail.com>
2021-03-30 19:09:26 +0530

    Initial Commit

---
+++ GitHub Example/DocumentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DocumentViewController.swift
+//  GitHub Example
+//
+//  Created by sidhu sidhu on 30/03/21.
+//
+
+import UIKit
+
+class DocumentViewController: UIViewController {
+    
+    @IBOutlet weak var documentNameLabel: UILabel!
+    
+    var document: UIDocument?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Access the document
+        document?.open(completionHandler: { (success) in
+            if success {
+                // Display the content of the document, e.g.:
+                self.documentNameLabel.text = self.document?.fileURL.lastPathComponent
+            } else {
+                // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
+            }
+        })
+    }
+    
+    @IBAction func dismissDocumentViewController() {
+        dismiss(animated: true) {
+            self.document?.close(completionHandler: nil)
+        }
+    }
+}
